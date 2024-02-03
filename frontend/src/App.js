228c64386d692ae374013d8e1708b1abf43793fd
Amit Kumar <amitkumar8789535280@gmail.com>
2024-02-03 18:46:01 +0530

    Header and Footer added inot App Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Header from './component/Header';
+import Footer from './component/Footer';
+import { Container} from 'react-bootstrap';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <h1>Welcome To ProShop</h1>
+      </main>
+      <Footer />
+    </>
+
   )
 }
 
