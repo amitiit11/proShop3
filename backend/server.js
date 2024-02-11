172d23205f54b48f7e1d5f13998433b19522d81b
Amit Kumar <amitkumar8789535280@gmail.com>
2024-02-11 10:55:46 +0530

    moved routing in separate file

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js';
+import connectDB from './config/db.js';
+
+import productRoutes from './routes/productRoutes.js'
 const port = process.env.PORT || 5000;
+
+connectDB();
 const app = express();
 app.get("/", (req, res)=>{
     res.send("API is running");
 })
-app.get("/api/products", (req, res)=>{
-    res.json(products);
-})
-app.get("/api/products/:id", (req, res)=>{
-    const product = products.find(productId => req.params.id === productId._id)
-    res.json(product);
-})
+app.use("/api/products", productRoutes)
 app.listen(port,()=>{
     console.log(`server is running at ${port}`);
 })
