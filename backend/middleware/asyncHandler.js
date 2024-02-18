cff901484890cda2d1ec9f5aaea834bec7b95020
Amit Kumar <amitkumar8789535280@gmail.com>
2024-02-18 23:55:34 +0530

    async handler

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = fn => (req, res, next) => {
-    Promise.resolve(fn(req,res,next)).catch(next);
+    Promise.resolve(fn(req,res,next)).catch(next); // passing error to the express
 }
 
 export default asyncHandler;
