7ee5104cf7ebb72a404379a8e2082150accc3bf2
Amit Kumar <amitkumar8789535280@gmail.com>
2024-02-18 23:59:14 +0530

    Homscreen updated with redux dev tool

--- frontend/src/screen/HomeScreen.jsx
+++ frontend/src/screen/HomeScreen.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Row, Col } from 'react-bootstrap';
-// import products from '../products';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Product from '../component/Product';
-
+import Loader from "../component/Loader";
+import Message from "../component/Message";
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-            // You can await here
-            const {data} = await axios.get('/api/products');
-            setProducts(data);
-            // ...
-        }
-        fetchData();
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-    }, [])
-    return (
+  return (
+    <>
+      {isLoading ? (
+        <Loader/>
+      ) : error ? (
+        <Message varient='danger'>{error?.data?.message || error.error}</Message>
+      ) : (
         <>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map((product) => {
-                    return (<Col key={product._id} sm={12} md={6} xl={3}>
-                        {/* <h3>{product.name}</h3> */}
-                        <Product product={product} />
-                    </Col>)
-
-                })}
-            </Row>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => {
+              return (
+                <Col key={product._id} sm={12} md={6} xl={3}>
+                  {/* <h3>{product.name}</h3> */}
+                  <Product product={product} />
+                </Col>
+              );
+            })}
+          </Row>
         </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
