7f6faef7db5db23d3b7523fb55c821791363c4e3
Amit Kumar <amitkumar8789535280@gmail.com>
2024-02-28 21:58:27 +0530

    reducer added in cartSlice

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -5,7 +5,6 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
-
 const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -15,7 +14,7 @@ const cartSlice = createSlice({
       const existingItem = state.cartItems.find((x) => x._id === item._id);
 
       if (existingItem) {
-        state.cartItems = state.cartItems((x) =>
+        state.cartItems = state.cartItems.map((x) =>
           x._id === existingItem._id ? item : x
         );
       } else {
@@ -23,8 +22,12 @@ const cartSlice = createSlice({
       }
       return updateCart(state);
     },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+      return updateCart(state);
+    },
   },
 });
 
-export const {addToCart}  = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
