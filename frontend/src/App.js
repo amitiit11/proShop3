00b226a81f7076e88bd23f8471a614c4bc186692
Amit Kumar <amitkumar8789535280@gmail.com>
2024-03-18 22:55:26 +0530

    ToastContainer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import Header from './component/Header';
-import Footer from './component/Footer';
-import { Container} from 'react-bootstrap';
-import {Outlet} from 'react-router-dom'
+import React from "react";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
           <Outlet />
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
