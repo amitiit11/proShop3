9043dc2f16e6dc213060c696ffe7dfd36d43dd03
Amit Kumar <amitkumar8789535280@gmail.com>
2024-04-16 22:40:21 +0530

    productApi Slice added

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,23 +1,42 @@
 import { PRODUCTS_URL } from "../constants";
-import { apiSlice } from './apiSlices';
+import { apiSlice } from "./apiSlices";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        getProducts: builder.query({
-            query: () => ({
-                url: PRODUCTS_URL,
-            }),
-            keepUnusedDataFor: 5,
-        }),
-        getProductDetails: builder.query({
-            query: (productId) => ({
-                url: `${PRODUCTS_URL}/${productId}`,
-            }),
-            keepUnusedDataFor: 5,
-        }),
-
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL,
+      }),
+      providesTags: ['Product'],
+      keepUnusedDataFor: 5,
     }),
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: PRODUCTS_URL,
+        method: "POST",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    updateProduct: builder.mutation({
+        query: (data) => ({
+            url: `${PRODUCTS_URL}/${data.productId}`,
+            method: 'PUT',
+            body: data,
+        }),
+        invalidatesTags: ["Product"],
+    })
+  }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
-
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+} = productsApiSlice;
