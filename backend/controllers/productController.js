6dbb362523f82b8c6d4fe5596c464dca7e0cc15b
Amit Kumar <amitkumar8789535280@gmail.com>
2024-04-16 22:23:18 +0530

    product controller file   added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,23 +4,65 @@ import asyncHandler from "../middleware/asyncHandler.js";
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
-const getProducts = asyncHandler(async(req,res)=>{
-    const products = await Product.find({});
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
 
-    res.json(products);
-})
+  res.json(products);
+});
 
 // @desc Fetch a products
 // @route GET /api/products/:id
 // @access Public
-const getProductById = asyncHandler(async(req,res)=>{
-    const product = await Product.findById(req.params.id)
-    if(product){
-        res.json(product);
-    }else{
-        res.status(404);
-       throw new Error('Resource not found');
-    }
-})
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+// @desc Create a products
+// @route POST /api/products
+// @access private/admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample Brand",
+    category: "sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
 
-export {getProductById, getProducts};
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.send(201).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("resource not found");
+  }
+});
+
+export { getProductById, getProducts, createProduct, updateProduct };
