c0c23ea62852d8e27184cda28815d81727dee166
Amit Kumar <amitkumar8789535280@gmail.com>
2024-04-21 00:39:43 +0530

    creating route for deleting of product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -65,4 +65,24 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProductById, getProducts, createProduct, updateProduct };
+// @desc Delete a product
+// @route DELETE /api/products/:id
+// @access Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await Product.deleteOne({ _id: product._id });
+    res.send(200).json("Resource not found");
+  } else {
+    res.status(404);
+    throw new Error("resource not found");
+  }
+});
+
+export {
+  getProductById,
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
